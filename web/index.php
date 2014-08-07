02b3140284bb0b252a1161b544da4e1e4607f1c9
Michael Gerstmann <mg@bytepark.de>
2014-08-07 18:46:25 +0200

    [beta] add read more links

--- web/index.php
+++ web/index.php
@@ -12,8 +12,7 @@
                     </div>
                     <div class="col-lg content content-main">
                         <p>Functional programming is the mustachioed hipster of programming paradigms. Originally relegated to the annals of computer science academia, functional programming has had a recent renaissance that is due largely to its utility in distributed systems (and probably also because “pure” functional languages like Haskell are difficult to grasp, which gives them a certain cache).</p>
-                        <h2>Headline</h2>
-                        <p>Stricter functional programming languages are typically used when a system’s performance and integrity are both critical — i.e. your program needs to do exactly what you expect every time and needs to operate in an environment where its tasks can be shared across hundreds or thousands of networked computers.</p>
+                        <a href="detail.php" class="link-reset btn">Read more</a>
                     </div>
                 </article>
             </li>
@@ -26,6 +25,7 @@
                         <p>Functional programming is the mustachioed hipster of programming paradigms. Originally relegated to the annals of computer science academia, functional programming has had a recent renaissance that is due largely to its utility in distributed systems (and probably also because “pure” functional languages like Haskell are difficult to grasp, which gives them a certain cache).</p>
                         <h2>Headline</h2>
                         <p>Stricter functional programming languages are typically used when a system’s performance and integrity are both critical — i.e. your program needs to do exactly what you expect every time and needs to operate in an environment where its tasks can be shared across hundreds or thousands of networked computers.</p>
+                        <a href="detail.php" class="link-reset btn">Read more</a>
                     </div>
                 </article>
             </li>
