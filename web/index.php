6b2bb4d683a805f46d8ae859fb2f1c38b5c21776
Michael Gerstmann <mg@bytepark.de>
2014-08-07 20:04:58 +0200

    [beta] add newslist styles

--- web/index.php
+++ web/index.php
@@ -11,8 +11,13 @@
                         <?php require('inc/_content-article--header.php'); ?>
                     </div>
                     <div class="col-lg content content-main">
+                        <figure> <img src="/assets/img/sculpture-368684_1280.jpg" alt="Bildtitel" />
+    <figcaption>Skulptur Statue Figur Kunst Personen Frauen Frau. Lizenz: Public Domain CC0 von <i class="copyright" data-quote="&copy;">Antranias</i></figcaption>
+</figure>
                         <p>Functional programming is the mustachioed hipster of programming paradigms. Originally relegated to the annals of computer science academia, functional programming has had a recent renaissance that is due largely to its utility in distributed systems (and probably also because “pure” functional languages like Haskell are difficult to grasp, which gives them a certain cache).</p>
+                         <div style="position:relative; z-index:1">
                         <a href="detail.php" class="link-reset btn">Read more</a>
+                        </div>
                     </div>
                 </article>
             </li>
