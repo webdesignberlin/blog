53678d088c182389e681107430a5b83a81f3a6d6
Michael Gerstmann <mg@bytepark.de>
2014-08-14 11:01:45 +0200

    [beta] add copyright and design/frontend info to footer

--- web/inc/footer.php
+++ web/inc/footer.php
@@ -1,3 +1,4 @@
-<footer>
-    <p class="container text-right"><a href="#top" class="top" title="Jump to the top of the page">&uarr; Back to top</a></p>
+<footer role="contentinfo">
+    <p class="container text-right clearfix"><a href="#top" class="top" title="Jump to the top of the page">&uarr; Back to top</a></p>
+    <p class="container clearfix">&copy; <strong>Mathias Witt</strong> || Design + Frontend: <a href="http://micha.seiler-gerstmann.de">Webdesign Berlin - Michael Gerstmann</a></p>
 </footer>
