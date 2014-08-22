6ddef97751af1f4bf5a5bbd44f0eaf8862910fca
webdesignberlin <reg@seiler-gerstmann.de>
2014-08-22 18:04:49 +0200

    [beta] update head
    config title

--- web/inc/head.php
+++ web/inc/head.php
@@ -2,7 +2,7 @@
 <html lang="en-US" prefix="og: http://ogp.me/ns#">
 <head>
     <meta charset="utf-8">
-    <title>Artikel</title>
+    <title>[TODO->title] [TODO->cat]</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     
     <!-- Favicon -->
